46748684fd389cfb2b619c42523b0e2f6b4c37e6
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-14 20:01:03 -0500

    Controlador de para login, register user y profile, como tambien middlewares para auth

---
+++ src/middlewares/ErrorMiddleware.js
@@ -0,0 +1,10 @@
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(`[ERROR] ${err.message}`); // Log interno para el desarrollador
+
+  res.status(statusCode).json({
+    status: "error",
+    statusCode: statusCode,
+    message: err.message || "Ocurrió un error inesperado en el servidor",
+  });
+}
---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken";
+import prisma from "../../prismaClient";
+
+export const protect = async (req, res, next) => {
+  let token;
+
+  // Lee el jwt token de la cookie
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await prisma.usuarios.findUnique({
+        where: { id: decoded.userId },
+        select: {
+          nombre: true,
+          email: true,
+          roles_id: true,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("No autorizado, token fallido !");
+    }
+  } else {
+    res.status(401);
+    throw new Error("No autorizado, no hay token!");
+  }
+};
+
+export const tendero = (req, res, next) => {
+    const rol_tendero = await prisma.roles.findFirst({where: {
+        nombre: "Tendero"
+    }});
+
+    if(req.user && req.user.roles_id == rol_tendero.id){
+        res.status(401);
+        throw new Error("No autorizado, debe ser Tendero")
+    }
+}
+
+export const admin = (req, res, next) => {
+    const rol_tendero = await prisma.roles.findFirst({where: {
+        nombre: "Admin"
+    }});
+
+    if(req.user && req.user.roles_id == rol_tendero.id){
+        res.status(401);
+        throw new Error("No autorizado, debe ser ADMIN")
+    }
+}
